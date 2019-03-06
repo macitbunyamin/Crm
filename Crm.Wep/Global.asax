0b890cd869f5d5855801841098b50b4cb27f2519
Bünyamin Macit <macitbunyamin@gmail.com>
2019-03-06 14:16:18 +0300

    Crm tema giydirildi ve admin wep katmanında oluşturuldu

---
+++ Crm.Wep/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crm.Wep.MvcApplication" Language="C#" %>
---
+++ Crm.Wep/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crm.Wep.Startup))]
+namespace Crm.Wep
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
